b53aecd971a0f48bc33750d92e6f1b81e4f7abb1
inayat100 <68042481+inayat100@users.noreply.github.com>
2022-03-06 12:11:15 +0530

    Update parking.py
    done

--- parking.py
+++ parking.py
@@ -1,27 +1,45 @@
 import time
-class park():
+class Parking ():
     def __init__(self):
         self.lt= []
-        print(f"we have only slot {slot}")
 
     def slot_add(self):
         global slot
+        global total
         t = time.localtime()
         tm = f'{t.tm_hour}:{t.tm_min}:{t.tm_sec}'
         while True:
             try:
                 if slot > 0:
+                    print('''
+                                   1 for car parking  Rs.50     2 for bike parking  Rs.20
+                                   3 for bicycle parking Rs.10   4 for trucks parking Rs.100
+                                   ''')
+                    ty = int(input("enter parking type..."))
+                    if ty == 1:
+                        am = 50
+                    elif ty == 2:
+                        am = 20
+                    elif ty == 3:
+                        am = 10
+                    elif ty == 4:
+                        am = 100
+                    else:
+                        print("please enter right type...")
+                        continue
                     self.id = input("enter vehicle id...")
                     if self.uniq():
-                        print("this id is exist take uniq id....")
+                        print("this id is exist  take unique id....")
                         continue
-                    self.name = input("enter vehicle name...")
-                    self.obj = {'id':int(self.id),'name':self.name,'time':tm}
+                    self.name = input("enter vehicle name or number...")
+                    self.obj = {'id':int(self.id),'name':self.name,'amount':am,'time':tm}
                     self.ad = self.lt.append(self.obj)
+                    print(f"for this parking amount : {am}")
+                    total = total + am
                     slot = slot - 1
                     print(f"we have only slot {slot}")
                     print("")
-                    y = input("for more add press Y and for exit presss any key")
+                    y = input("for more add press Y/y and for exit presss any key")
                     print("")
                     if y == 'y' or y == 'Y':
                         continue
@@ -33,31 +51,34 @@ class park():
                     print("sloat is full")
                     print("")
                     break
+
             except Exception:
-                print(" maybe your your input is wrong....")
+                print("maybe your  input is not right....")
 
     def slot_remove(self):
         global slot
+        global total
         try:
             self.id = input("enter vehicle id")
             for l in self.lt:
                 if l['id'] == int(self.id):
                     self.r = self.lt.index(l)
-
                     self.lt.remove(self.lt[self.r])
+                    total = total - l['amount']
                     slot = slot + 1
                     print("removed....")
                     break
             else:
-                print("id not matched.....")
+                print("id not matched..... enter right id")
         except Exception:
-            print("maybe your your input is wrong....")
+            print("maybe  your input is not right....")
 
     def view(self):
-        print("view....")
-        print("Id " ,"  Name",  "    Time")
+        print('''
+            Id   Number   Amount   Time ''')
         for l in self.lt:
-            print( f"{l['id']}  , {l['name']} ,   {l['time']}")
+            print( f'''
+            {l['id']}   {l['name']}    {l['amount']}    {l['time']} ''')
 
     def uniq(self):
          for l in self.lt:
@@ -67,21 +88,23 @@ class park():
 
 if __name__ == '__main__':
     slot= 4
-    p = park()
+    total = 0
+    p = Parking()
     while True:
+        print(f"we have total parking  slot {slot}    Total amount {total}")
         print('''
-        1 add slot
-        2 remove slot 
-        3 show all slot
+        1 Add parking vehicle
+        2 Remove parking vehicle 
+        3 View all vehicle detail
         ''')
         try:
-            op = input("choice optins")
+            op = input("what do you want perform Choice a number")
             print("")
             if int(op) == 1:
                 p.slot_add()
             elif int(op)  == 2:
                 p.view()
-                print("just enter id for delete")
+                print("enter right id for remove parking vehicle")
                 p.slot_remove()
             elif int(op)  == 3:
                 p.view()
